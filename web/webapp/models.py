5f1a5c91b550d105f02cfb757e4bcdce6bebe0ff
Rahul <84090625+Rah200@users.noreply.github.com>
2023-02-01 10:07:09 +0530

    Delete models.py

--- web/webapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-    is_admin = models.BooleanField('Is admin', default=False)
-    is_doctor = models.BooleanField('Is doctor', default=False)
-    is_patient = models.BooleanField('Is patient', default=False)
-    profile_image = models.ImageField(null=True, blank=True)
